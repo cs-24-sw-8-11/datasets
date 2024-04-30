6636e24597c19a132e295f5aa22723be0f579923
mast3r <thomasmollerjensen@live.dk>
2024-04-30 15:22:30 +0200

    changed to a different model

--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 download the dataset we've used from https://www.kaggle.com/datasets/lucasgreenwell/depression-anxiety-stress-scales-responses
+
+https://huggingface.co/lxyuan/distilbert-base-multilingual-cased-sentiments-student
--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,18 +1,24 @@
 import sys
 import transformers
 
-sentiment_pipeline = transformers.pipeline("sentiment-analysis")
+sentiment_pipeline = transformers.pipeline(model="lxyuan/distilbert-base-multilingual-cased-sentiments-student")
 
 res = sentiment_pipeline(sys.argv[1:])
 
+print(res)
+
 for result in res:
     if result['score'] < .5:
-        if result["label"] == "POSITIVE":
-            print(result['score']+1.5) # low score should be neutral between 1 to 2, positive neutrals should be between 1.5 to 2
+        if result["label"].lower() == "positive":
+            # low score should be neutral between 1 to 2, positive neutrals should be between 1.5 to 2
+            print(result['score']+1.5) 
         else:
-            print(result['score']+1) # low score should be neutral between 1 to 2, negative neutrals should be between 1 to 1.5
+            # low score should be neutral between 1 to 2, negative neutrals should be between 1 to 1.5
+            print(1-(result['score']+1)) 
     else:
-        if result["label"] == "POSITIVE":
-            print(result['score']+2) # positive should be between 2 to 3
+        if result["label"].lower() == "positive":
+            # positive should be between 2 to 3
+            print(result['score']+2) 
         else:
-            print(result['score']) # negative should be between 0 to 1
+            # negative should be between 0 to 1
+            print(1-result['score']) 

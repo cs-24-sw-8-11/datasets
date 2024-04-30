23e65c7bcd71eb59f632289d50556cf47c7dd5cd
mast3r <thomasmollerjensen@live.dk>
2024-04-30 10:25:03 +0200

    updated printing format

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -6,4 +6,7 @@ sentiment_pipeline = transformers.pipeline("sentiment-analysis")
 res = sentiment_pipeline(sys.argv[1:])
 
 for result in res:
-    print(((1 if result['label'] == "POSITIVE" else 0) + result["score"])/2)
+    if result['label'] == "POSITIVE":
+        print(result['score'])
+    else:
+        print(-result['score'])
